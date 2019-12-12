e38ce748b306fdac018ccbb2f46a2bff6a80d55f
Digya Mandala Putra <digyamp@gmail.com>
2019-12-12 07:50:27 +0000

    update code

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
 class Main {
 
   /*
-   * Find LONGEST COMMON SUBSEQUENCE using Dynamic Programming Formula:
+   * Find LONGEST COMMON SUBSEQUENCE STRING using Dynamic Programming Formula:
    * 
-   * f(i,0) = f(0,j) = 0
+   * f(i,0) = f(0,j) = ''
    * 
-   * f(i,j) = max(f(i-1,j), f(i,j-1)) if A[i] == B[j]
-   * f(i,j) = f(i-1,j) + 1 if A[i] != B[j]
+   * if A[i-1] == B[j-1]
+   * f(i,j) = max(f(i-1,j), f(i,j-1))
+   * 
+   * if A[i-1] != B[j-1]
+   * f(i,j) = f(i-1,j) + 1
+   * 
+   * because in Array of A and B, indexing starts from 0
    */
-
   private static int[][] generate_LCS_table(String a, String b) {
     int[][] dp = new int[101][101];
 
@@ -28,6 +32,45 @@ class Main {
     return dp;
   }
 
+  /*
+   * Find LONGEST COMMON SUBSEQUENCE STRING using Dynamic Programming Formula:
+   * 
+   * f(i,0) = f(0,j) = ''
+   * 
+   * if A[i-1] == B[j-1]
+   * f(i,j) = max(f(i-1,j), f(i,j-1))
+   * 
+   * if A[i-1] != B[j-1]
+   * f(i,j) = f(i-1,j) + A[i-1]
+   * 
+   * because in Array of A and B, indexing starts from 0
+   */
+  private static String[][] generate_LCS_str_table(String a, String b) {
+    String[][] dpString = new String[101][101];
+
+    for (int i = 0; i <= 100; i++) {
+      dpString[i][0] = dpString[0][i] = "";
+    }
+
+    for (int i = 1; i <= a.length(); i++) {
+      for (int j = 1; j <= b.length(); j++) {
+        if (a.charAt(i - 1) != b.charAt(j - 1)) {
+          dpString[i][j] = max(dpString[i - 1][j], dpString[i][j - 1]);
+        } else {
+          dpString[i][j] = dpString[i - 1][j] + a.charAt(i - 1); // or b.charAt(j - 1)
+        }
+      }
+    }
+    return dpString;
+  }
+
+  private static String max(String a, String b) {
+    if (a.length() > b.length()) {
+      return a;
+    }
+    return b;
+  }
+
   public static void main(String[] args) {
 
     String a = "abcdef";
@@ -37,6 +80,8 @@ class Main {
     // Longest common subsequence is 'adef', should return 4
 
     int[][] dp = generate_LCS_table(a, b);
-    System.out.println(dp[a_length][b_length]);
+    String[][] dpStr = generate_LCS_str_table(a, b);
+    
+    System.out.println(dp[a_length][b_length] + ": " + dpStr[a_length][b_length]);
   }
 }
